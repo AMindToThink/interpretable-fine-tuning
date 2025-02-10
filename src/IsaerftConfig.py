8881c094b3f0e17ff3aac95c808bca856019e5f4
AMindToThink <matthewkhoriaty2026@u.northwestern.edu>
2025-02-10 23:47:07 +0000

    Start of the IsaerftConfig

---
+++ src/IsaerftConfig.py
@@ -0,0 +1,35 @@
+# Todo: make the dataclass extend PeftConfig and work out pushing the model to hub
+# from peft import PeftConfig
+
+from dataclasses import asdict, dataclass, field
+from typing import Union, List, Optional
+@dataclass
+class IsaerftConfig():#(PeftConfig):
+    """
+    This class will hold the hyperparameters of an [`ISaeRFTModel`]
+    
+    Args:
+        target_hooks (`Union[List[str], str]`):
+            Which of the HookedSAETransformer hook locations should have components added to them during fine-tuning
+            # Todo: the neat pattern-matching thing for selecting layers. Also the "exclude_modules" analogue.
+        depth (`int`):
+            How many layers for the FFNNs to have. -1 only trains a bias, 0 only trains a linear layer. defaults to 1.
+        hidden_size (`Optional[int]`):
+            The hidden size of the FFNN. depth must be > 0 for this to be provided.
+        lora_r (`Optional[int]`):
+            The rank of the matrices for the FFNN. Depth must be greater than -1. # Todo: find good hyperparameters for the defaults
+        # Todo: add args initialization type, dropout, checkpointing, and other parameters supported in the LoraConfig https://github.com/huggingface/peft/blob/main/src/peft/tuners/lora/config.py
+    """
+    target_hooks:Union[List[str], str] = field()
+    depth:int = field(default=1, metadata={"help": "How many layers for the FFNNs to have. -1 only trains a bias, 0 only trains a linear layer. defaults to 1."})
+    hidden_size:Optional[int] = field(default=128, metadata={'help':'The hidden size of the FFNN. depth must be > 0 for this to be provided.'})
+    lora_r:Optional[int] = field(default=64, metadata={'help':'The rank of the matrices for the FFNN. Depth must be greater than -1.'}) # Todo: find an actually good lora_r value here
+    def __post_init__(self):
+        # super().__post_init__() include once extending PeftConfig
+        if self.depth < 1 and (self.hidden_size is not None):
+            raise ValueError("`hidden_size` must be None if there are no hidden parameters! (ie, when `depth` is less than 1)")
+        if self.depth < 0 and (lora_r is not None):
+            raise ValueError("`lora_r` must be None if there are no matrices! (ie, when `depth` is less than 0)")
+
+
+
