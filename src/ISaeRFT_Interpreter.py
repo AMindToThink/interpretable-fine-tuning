02941958665f6e3d62faa0cd5532937a6c7e14bf
AMindToThink <matthewkhoriaty@gmail.com>
2025-02-18 21:05:07 +0000

    Add assertions to check that things are ok

--- src/ISaeRFT_Interpreter.py
+++ src/ISaeRFT_Interpreter.py
@@ -1,11 +1,15 @@
+# Standard imports
 import os
 import torch
 from torch import Tensor
 from torch.nn import Linear
+# Odd imports
+from NeuronpediaClient import NeuronpediaClient
+from sae_lens import SAE
+# Own code
 from model_components.LoRALinear import LoRALinear
 from model_components.ResidualBlock import ResidualBlock
 from model_components.BiasOnly import BiasOnly
-from NeuronpediaClient import NeuronpediaClient
 class ISaeRFT_Interpreter():
     """
         Class that turns the learned parameters into human-readable algorithms and outputs.
@@ -17,11 +21,14 @@ class ISaeRFT_Interpreter():
         Predicted help:
         If your outputs don't make sense, try checking that the release_id and sae_id are correct and applying L1 loss to the training to encourage sparsity.
     """
-    def __init__(self, model_id:str, layer:str, neuronpedia_api_key:str|None=None):
+    def __init__(self, model_id:str, layer:str, sae:SAE|None, release:str='', sae_id:str='', neuronpedia_api_key:str|None=None):
+        assert (sae is None) != (release and sae_id), "Must either give an sae or a release and sae_id and not both."
+        assert not (release and sae_id) or sae_id.split('/')[0] == layer, "Clearly one of us doesn't understand the relationship between sae_id and layer. Please check!"
         self.model_id = model_id
         # self.release_id = release_id
         self.layer = layer
         self.neuronpedia_client = NeuronpediaClient(api_key=os.environ['NEURONPEDIA_API_KEY'] if neuronpedia_api_key is None else neuronpedia_api_key)
+        self.sae = sae if sae is not None else SAE.from_pretrained(release=release, sae_id=sae_id, device='cpu') # If vRAM becomes an issue, could try putting this on cpu instead, since it isn't involved in much .
 
     @property
     def bias_interpretation_types(self):
@@ -39,19 +46,21 @@ class ISaeRFT_Interpreter():
         # Decided to go with the vectors instead of the components.
         # assert len(bias.shape) == 1, "Can only interpret bias vectors with interpret_bias. That means the shape input must be rank 1."
         assert interpretation_type in self.bias_interpretation_types, f"No such interpretation type '{interpretation_type}'. Must be in {self.bias_interpretation_types}"
+        assert len(vector.shape) == 1, "Can only interpret bias vectors with interpret_bias. That means the shape input must be rank 1." 
+        assert self.sae.cfg.d_sae == len(vector), "The dimension of the sae and the length of the bias vector do not match."
 
         if interpretation_type == 'absolute':
             # Sort indices by absolute values in descending order
             sorted_indices = torch.argsort(torch.abs(vector), descending=True)
         
-        if interpretation_type == 'expectation':
-            for i, value in enumerate(vector):
-                client_json = self.neuronpedia_client.get_feature(model_id=self.model_id, layer=self.layer, index=i)
-                freq_hist_data_bar_values = client_json['freq_hist_data_bar_values']
-                freq_hist_data_bar_heights = client_json['freq_hist_data_bar_heights']
+        # if interpretation_type == 'expectation':
+        #     for i, value in enumerate(vector):
+        #         client_json = self.neuronpedia_client.get_feature(model_id=self.model_id, layer=self.layer, index=i)
+        #         freq_hist_data_bar_values = client_json['freq_hist_data_bar_values']
+        #         freq_hist_data_bar_heights = client_json['freq_hist_data_bar_heights']
                 
 
-            raise NotImplementedError("Need to figure out how to manage this long-tail-looking distribution")
+        #     raise NotImplementedError("Need to figure out how to manage this long-tail-looking distribution")
         
         indices_to_check = sorted_indices[:top_k]
         feature_results = []
